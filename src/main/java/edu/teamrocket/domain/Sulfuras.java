0c1f2cf98a79dda605868e98110f00292f43bf67
Adriceka <adrigonzalezgonzalez2007@gmail.com>
2026-03-17 11:29:17 +0100

    feat: Implementar la clase pública Sulfuras
    Implementar Sulfuras como objeto legendario sin degradarlo

---
+++ src/main/java/edu/teamrocket/domain/Sulfuras.java
@@ -0,0 +1,11 @@
+package edu.teamrocket.domain;
+
+public class Sulfuras extends NormalItem{
+
+    public Sulfuras(String name, int quality, int sellIn) {
+        super(name, quality, sellIn);
+    }
+
+    public void updateQuality() {
+    }
+}
