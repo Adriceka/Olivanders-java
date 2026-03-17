902c81b1e02a08292d171a636f94c7181b0b19c9
Adriceka <adrigonzalezgonzalez2007@gmail.com>
2026-03-17 11:30:27 +0100

    feat: Implementar la clase pública Backstage
    Implementar Backstage con lógica de calidad por niveles

---
+++ src/main/java/edu/teamrocket/domain/Backstage.java
@@ -0,0 +1,20 @@
+package edu.teamrocket.domain;
+
+public class Backstage extends NormalItem{
+    public Backstage(String name, int quality, int sellIn) {
+        super(name, quality, sellIn);
+    }
+
+    @Override
+    public void updateQuality() {
+        if (this.getSellIn() > 10) {
+            this.computeQuality(this.getQuality() + 1);
+        } else if (this.getSellIn() > 5) {
+            this.computeQuality(this.getQuality() + 2);
+        } else if (this.getSellIn() > 0) {
+            this.computeQuality(this.getQuality() + 3);
+        } else {
+            this.computeQuality(0);
+        }
+    }
+}
