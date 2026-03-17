b3715860fec60e0fedb36e77003d3aae12bb6e25
Adriceka <adrigonzalezgonzalez2007@gmail.com>
2026-03-17 11:26:44 +0100

    feat: Implementar NormalItem
    Implemenar NormalItem usando composición con Item

---
+++ src/main/java/edu/teamrocket/domain/NormalItem.java
@@ -0,0 +1,52 @@
+package edu.teamrocket.domain;
+
+public class NormalItem implements Updateable{
+
+        private String name;
+        private int quality;
+        private int sellIn;
+        private Item item;
+
+    public NormalItem(String name, int quality, int sellIn) {
+
+        this.item=new Item(name, quality, sellIn);
+
+    }
+
+    protected Item getItem() {
+        return this.item;
+    }
+
+    public String getName() {
+       return this.item.getName();
+    }
+    
+    public int getQuality() {
+        return this.item.getQuality();
+    }
+
+    public int getSellIn() {
+        return this.item.getSellIn();
+    }
+
+    public void setSellIn(int sellIn) {
+        this.item.setSellIn(sellIn);
+    }
+
+    @Override
+    public void updateQuality() {
+
+    }
+
+    protected void computeQuality(int quality) {
+        this.item.setQuality(quality);
+    }
+
+    public String toString() {
+        return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+
+
+
+}
