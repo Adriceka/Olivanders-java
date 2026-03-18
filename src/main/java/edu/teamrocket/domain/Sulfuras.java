d0caec77b1dd7bd840358f212cf0b91c84b5eed0
Pablo Gonzalez Temes <pablogonzaleztemes@gmail.com>
2026-03-18 13:22:32 +0100

    fix(Sulfuras): :ambulance: Arreglar orden de los parámetros en el constructor

--- src/main/java/edu/teamrocket/domain/Sulfuras.java
+++ src/main/java/edu/teamrocket/domain/Sulfuras.java
@@ -2,7 +2,7 @@ package edu.teamrocket.domain;
 
 public class Sulfuras extends NormalItem{
 
-    public Sulfuras(String name, int quality, int sellIn) {
+    public Sulfuras(String name, int sellIn, int quality) {
         super(name, quality, sellIn);
     }
 
