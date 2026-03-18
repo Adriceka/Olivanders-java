5c981c611ce62a46c659121f5253a6bbae7d2ebd
Pablo Gonzalez Temes <pablogonzaleztemes@gmail.com>
2026-03-18 14:20:31 +0100

    feat(Conjured): :construction: Añadir clase Conjured

---
+++ src/main/java/edu/teamrocket/domain/Conjured.java
@@ -0,0 +1,18 @@
+package edu.teamrocket.domain;
+
+public class Conjured extends NormalItem {
+
+    public Conjured (String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+    @Override
+    public void updateQuality() {
+        if (getSellIn() > 0) {
+            computeQuality(2);
+        } else {
+            computeQuality(4);
+        }
+        setSellIn();
+    }
+}
+
