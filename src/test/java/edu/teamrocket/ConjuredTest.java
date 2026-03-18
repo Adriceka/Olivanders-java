efffb33296639bcfcbb6d075ba151d9e02735b48
Pablo Gonzalez Temes <pablogonzaleztemes@gmail.com>
2026-03-18 14:23:40 +0100

    test(ConjuredTest): :white_check_mark: añadir casos test para la clase Conjured

---
+++ src/test/java/edu/teamrocket/ConjuredTest.java
@@ -0,0 +1,66 @@
+package edu.teamrocket;
+
+import org.junit.jupiter.api.Test;
+import edu.teamrocket.domain.Conjured;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ConjuredTest {
+
+    @Test
+    public void crearConjured() {
+
+        Conjured conjured = new Conjured("Conjured Mana Cake", 6, 3);
+        assertEquals("Conjured Mana Cake", conjured.getName());
+        assertEquals(3, conjured.getSellIn(), 0);
+        assertEquals(6, conjured.getQuality(), 0);
+    }
+
+    @Test
+    public void toStringTest() {
+        Conjured conjured = new Conjured("Conjured Mana Cake", 6, 3);
+        System.out.println("toString() Conjured test:");
+        System.out.println(conjured.toString());
+    }
+
+    @Test
+    public void updateQualityConjured() {
+
+        Conjured conjured = new Conjured("Conjured Mana Cake", 6, 3);
+        conjured.updateQuality();
+        assertEquals(2, conjured.getSellIn(), 0);
+        assertEquals(4, conjured.getQuality(), 0);
+    }
+
+    @Test
+    public void updateQualityConjuredJustExpired() {
+
+        Conjured conjured = new Conjured("Conjured Mana Cake", 0, 6);
+        conjured.updateQuality();
+        assertEquals(-1, conjured.getSellIn(), 0);
+        assertEquals(2, conjured.getQuality(), 0);    
+    }
+
+    @Test
+    public void updateQualityConjuredExpired() {
+
+        Conjured conjured = new Conjured("Conjured Mana Cake", -1, 6);
+        conjured.updateQuality();
+        assertEquals(-2, conjured.getSellIn(), 0);
+        assertEquals(2, conjured.getQuality(), 0);
+    }
+
+    @Test
+    public void qualityMinZERO() {
+
+        Conjured brie = new Conjured("Conjured Mana Cake", 1, 1);
+        brie.updateQuality();
+        assertEquals(0, brie.getSellIn(), 0);
+        assertEquals(0, brie.getQuality(), 0);
+
+        brie = new Conjured("Conjured Mana Cake", -1, 0);
+        brie.updateQuality();
+        assertEquals(-2, brie.getSellIn(), 0);
+        assertEquals(0, brie.getQuality(), 0);
+    }
+}
